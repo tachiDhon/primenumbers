b63198f2a8e590bf73d1660e743eda25dfa016f2
TashiDHUNDUP <tashi.dhundup@epitech.eu>
2020-02-11 12:14:33 +0100

    ajout github

--- primenumbers.php
+++ primenumbers.php
@@ -1,32 +1,28 @@
 <?php
-function prime_numbers($n, $i, $flag) {
+function prime_numbers($n) {
     $flag=1;
     $i=2;
-    //Read: $n;
 
     if ($n<=1) {
         return false;
-    }
-    else {
-        goto Step7; // Any number below 1 is not prime
-    
-        while ($i < [($n/2) + 1]) {
-            
-            if ($n%$i==0) {
-                $flag==0;
-            }
-            goto step6;
-        }
-        
-    } 
-    Step7:
-    echo 'Stop'; 
-
-    step6:
-    if ($flag==0) {
-        return false;
     } else {
-        return true;
+        //goto Step7; Any number below 1 is not prime
+        while ($i < ($n/2) + 1) {
+            if ($n%$i==0) {
+                return false;
+                // $flag=0;
+                // continue;
+            }
+            // goto step6;
+            $i++;
+        }
     }
+    // step6:
+    return true;
 }
-prime_numbers(2, 0, $flag);
+var_dump(prime_numbers(31));
+var_dump(prime_numbers(6));
+var_dump(prime_numbers(7));
+var_dump(prime_numbers(13));
+var_dump(prime_numbers(14));
+var_dump(prime_numbers(100));
